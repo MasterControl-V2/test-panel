ded1fba3363200cbf7edb373430abfa5bb884380
MasterControl-V2 <mycontrol.bot2@gmail.com>
2026-05-05 04:14:03 +0700

    Update udp.sh

--- udp.sh
+++ udp.sh
@@ -1568,10 +1568,10 @@ EOF
 echo -e "${Y}🌐 Network Configuration ပြုလုပ်နေပါတယ်...${Z}"
 
 # ===== UDP CONNECTION TRACKING TIMEOUT FIX =====
-sysctl -w net.netfilter.nf_conntrack_udp_timeout=0
-sysctl -w net.netfilter.nf_conntrack_udp_timeout_stream=0
-grep -q '^net.netfilter.nf_conntrack_udp_timeout=0' /etc/sysctl.conf || echo 'net.netfilter.nf_conntrack_udp_timeout=0' >> /etc/sysctl.conf
-grep -q '^net.netfilter.nf_conntrack_udp_timeout_stream=0' /etc/sysctl.conf || echo 'net.netfilter.nf_conntrack_udp_timeout_stream=0' >> /etc/sysctl.conf
+sysctl -w net.netfilter.nf_conntrack_udp_timeout=8640
+sysctl -w net.netfilter.nf_conntrack_udp_timeout_stream=8640
+grep -q '^net.netfilter.nf_conntrack_udp_timeout=8640' /etc/sysctl.conf || echo 'net.netfilter.nf_conntrack_udp_timeout=8640' >> /etc/sysctl.conf
+grep -q '^net.netfilter.nf_conntrack_udp_timeout_stream=8640' /etc/sysctl.conf || echo 'net.netfilter.nf_conntrack_udp_timeout_stream=8640' >> /etc/sysctl.conf
 
 sysctl -w net.ipv4.ip_forward=1 >/dev/null
 grep -q '^net.ipv4.ip_forward=1' /etc/sysctl.conf || echo 'net.ipv4.ip_forward=1' >> /etc/sysctl.conf
