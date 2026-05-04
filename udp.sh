cbf76acc720f8447054ea434a9be8dc4ad2a30bd
MasterControl-V2 <mycontrol.bot2@gmail.com>
2026-05-04 18:20:10 +0700

    Update udp.sh

--- udp.sh
+++ udp.sh
@@ -233,9 +233,9 @@ if jq . >/dev/null 2>&1 <<<'{}'; then
     .listen = ":5667" |
     .cert = "/etc/zivpn/zivpn.crt" |
     .key  = "/etc/zivpn/zivpn.key" |
-    .obfs = "random" |
+    .obfs = "wechat" |
     .mux = true |
-    .mux_concurrency = 1440 |
+    .mux_concurrency = 8640 |
     .server = $ip
   ' "$CFG" > "$TMP" && mv "$TMP" "$CFG"
 fi
