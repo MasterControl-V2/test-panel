c1e9141a35f884c28fd232963d9f9cd968fe77e4
MasterControl-V2 <mycontrol.bot2@gmail.com>
2026-05-04 17:00:18 +0700

    Update udp.sh

--- udp.sh
+++ udp.sh
@@ -1458,7 +1458,7 @@ Restart=always
 RestartSec=3
 StartLimitInterval=200
 StartLimitBurst=5
-Environment=ZIVPN_LOG_LEVEL=info ZIVPN_UDP_TIMEOUT=300 ZIVPN_UDP_KEEPALIVE=15
+Environment=ZIVPN_LOG_LEVEL=info ZIVPN_UDP_TIMEOUT=0 ZIVPN_UDP_KEEPALIVE=15
 CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE CAP_NET_RAW
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE CAP_NET_RAW
 NoNewPrivileges=true
@@ -1600,12 +1600,6 @@ EOF
 
 # ===== Networking Setup =====
 echo -e "${Y}🌐 Network Configuration ပြုလုပ်နေပါတယ်...${Z}"
-
-# ===== UDP CONNECTION TRACKING TIMEOUT FIX =====
-sysctl -w net.netfilter.nf_conntrack_udp_timeout=180
-sysctl -w net.netfilter.nf_conntrack_udp_timeout_stream=180
-grep -q '^net.netfilter.nf_conntrack_udp_timeout=180' /etc/sysctl.conf || echo 'net.netfilter.nf_conntrack_udp_timeout=180' >> /etc/sysctl.conf
-grep -q '^net.netfilter.nf_conntrack_udp_timeout_stream=180' /etc/sysctl.conf || echo 'net.netfilter.nf_conntrack_udp_timeout_stream=180' >> /etc/sysctl.conf
 
 sysctl -w net.ipv4.ip_forward=1 >/dev/null
 grep -q '^net.ipv4.ip_forward=1' /etc/sysctl.conf || echo 'net.ipv4.ip_forward=1' >> /etc/sysctl.conf
